3f155f163ec7bcc18aa0f26083537e93cb7368d8
PuppyGamingDev <82091573+PuppyGamingDev@users.noreply.github.com>
2026-01-22 08:05:09 +0000

    Update deploy.bash

--- deployment/deploy.bash
+++ deployment/deploy.bash
@@ -34,11 +34,11 @@ if [ -f "/root/.ssh/authorized_keys" ]; then
   service ssh restart
 fi
 
-ufw default deny incoming
-ufw default allow outgoing
-ufw allow ssh
-ufw allow 443
-ufw --force enable
+# ufw default deny incoming
+# ufw default allow outgoing
+# ufw allow ssh
+# ufw allow 443
+# ufw --force enable
 
 cat >/root/.vimrc <<'EOF'
 colorscheme desert
@@ -117,11 +117,11 @@ systemctl restart nginx
 
 DEBIAN_FRONTEND=noninteractive apt-get install -y git make g++ cmake libssl-dev libunbound-dev libboost-dev libboost-system-dev libboost-date-time-dev libboost-dev libboost-system-dev libboost-date-time-dev libboost-filesystem-dev libboost-thread-dev libboost-chrono-dev libboost-locale-dev libboost-regex-dev libboost-regex-dev libboost-program-options-dev libzmq3-dev
 cd /usr/local/src
-git clone https://github.com/monero-project/monero.git
-cd monero
-git checkout v0.18.4.3
-git submodule update --init
-USE_SINGLE_BUILDDIR=1 make -j$(nproc) release || USE_SINGLE_BUILDDIR=1 make -j1 release
+# git clone https://github.com/monero-project/monero.git
+# cd monero
+# git checkout v0.18.4.5
+# git submodule update --init
+# USE_SINGLE_BUILDDIR=1 make -j$(nproc) release || USE_SINGLE_BUILDDIR=1 make -j1 release
 
 (cat <<EOF
 set -x
